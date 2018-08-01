f9c0f6e7928288599d413f8cf250e97736f72b94
gonstr <tom@fareoffice.com>
2018-08-02 00:18:03 +0200

    wip

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### TODO
 
 - alot
+- escape html?
--- terminal.js
+++ terminal.js
@@ -1,18 +1,40 @@
 const { exec } = require('child_process')
+const path = require('path')
+const os = require('os')
 
 const body = document.getElementsByTagName('body')[0]
 const main = document.getElementById('main')
+const output = document.getElementById('output')
+const dir = document.getElementById('dir')
 const input = document.getElementById('input')
 
-let proc
-let ctrlDown = false
+let proc,
+  nextCwd,
+  cwd = os.homedir(),
+  ctrlDown = false
 
 function appendRow(text = '&nbsp', color) {
   const pre = document.createElement('pre')
   pre.innerHTML = text
   if (color) pre.style.color = color
-  main.appendChild(pre)
+  output.appendChild(pre)
+  main.scrollTop = main.scrollHeight
 }
+
+function updateDir() {
+  dir.innerHTML = dirName()
+}
+
+function dirName() {
+  return `${
+    process
+      .cwd()
+      .split('/')
+      .reverse()[0]
+  }$&nbsp;`
+}
+
+updateDir()
 
 input.addEventListener('keydown', ({ key }) => {
   if (!proc && key === 'Enter') {
@@ -20,17 +42,24 @@ input.addEventListener('keydown', ({ key }) => {
     input.value = ''
 
     if (val) {
-      input.style.visibility = 'hidden'
-      proc = exec(val)
-      proc.stdout.on('data', data => appendRow(data))
-      proc.stderr.on('data', data => appendRow(data, '#ff5555'))
-      proc.on('close', () => {
-        input.style.visibility = 'visible'
-        input.focus()
-        proc = null
-      })
+      appendRow(dirName() + val)
+      const split = val.split(' ')
+      if (split[0].toLowerCase() === 'cd') {
+        process.chdir(split.slice(1).join(' '))
+        updateDir()
+      } else {
+        input.style.visibility = 'hidden'
+        proc = exec(val)
+        proc.stdout.on('data', data => appendRow(data.toString('utf8')))
+        proc.stderr.on('data', data => appendRow(data.toString('utf8'), '#ff5555'))
+        proc.on('close', () => {
+          input.style.visibility = 'visible'
+          input.focus()
+          proc = null
+        })
+      }
     } else {
-      appendRow()
+      appendRow(dirName())
     }
   }
 
